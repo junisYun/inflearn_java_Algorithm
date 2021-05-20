41e1905dc3f82fbd77a6820bba8048cd7e75fbdb
junisYun <junis.yun94@gmail.com>
2021-05-20 19:04:06 +0900

    2021_05_20 19:04

--- inflearn8_07/src/Main.java
+++ inflearn8_07/src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static int[][] arr = new int[50][50];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        System.out.println(dfs(n, r));
+    }
+
+    public static int dfs(int n, int r) {
+        if(n == r || r == 0) {
+            return 1;
+        } else if(arr[n][r] != 0) {
+            return arr[n][r];
+        } else{
+            return arr[n][r] = dfs(n - 1, r - 1) + dfs(n - 1, r);
+        }
     }
 }
