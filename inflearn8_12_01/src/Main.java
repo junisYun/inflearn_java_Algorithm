1b5715f5c29a2abda5c919b70d8d9888bb0755b6
junisYun <junis.yun94@gmail.com>
2021-05-21 14:56:50 +0900

    2021_05_21 14:56

--- inflearn8_12_01/src/Main.java
+++ inflearn8_12_01/src/Main.java
@@ -1,2 +1,64 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Pos {
+    int x, y;
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 public class Main {
+    static int[][] box;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int N, M;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        M = sc.nextInt();
+        N = sc.nextInt();
+        box = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                box[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(bfs());
+
+    }
+    public static int bfs() {
+        Queue<Pos> q = new LinkedList<>();
+        int max = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(box[i][j] == 1) {
+                    q.offer(new Pos(i, j));
+                }
+            }
+        }
+
+        while(!q.isEmpty()) {
+            Pos p = q.poll();
+            int x = p.x;
+            int y = p.y;
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if(box[nx][ny] == 0) {
+                        box[nx][ny] = box[x][y] + 1;
+                        q.offer(new Pos(nx, ny));
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(box[i][j] == 0) return -1;
+                max = Math.max(max, box[i][j]);
+            }
+        }
+        return max - 1;
+    }
 }
