f2453d47b32288f526e9b99e5eae77c95d34d5fe
junisYun <junis.yun94@gmail.com>
2021-05-24 16:28:12 +0900

    2021_05_24 16:28

---
+++ inflearn8_15/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Pos {
+    int x, y;
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+public class Main {
+    static int N, M;
+    static int[][] town;
+    static int len;
+    static int[] combi;
+    static int answer = Integer.MAX_VALUE;
+    static ArrayList<Pos> house = new ArrayList<>();
+    static ArrayList<Pos> pizza = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        town = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                town[i][j] = sc.nextInt();
+                if(town[i][j] == 1) {
+                    house.add(new Pos(i, j));
+                }
+                else if(town[i][j] == 2) {
+                    pizza.add(new Pos(i, j));
+                }
+            }
+        }
+        len = pizza.size();
+        combi = new int[M];
+        dfs(0, 0);
+        System.out.println(answer);
+    }
+    public static void dfs(int L, int s) {
+        if(L == M) {
+            int sum = 0;
+            for(int i = 0; i < house.size(); i++) {
+                int min = Integer.MAX_VALUE;
+                for(int j = 0; j < combi.length; j++) {
+                    min = Math.min(min, Math.abs(house.get(i).x - pizza.get(combi[j]).x) + Math.abs(house.get(i).y - pizza.get(combi[j]).y));
+                }
+                sum += min;
+            }
+            answer = Math.min(answer, sum);
+        } else {
+            for(int i = s; i < len; i++) {
+                combi[L] = i;
+                dfs(L + 1, i + 1);
+            }
+        }
+    }
+}
